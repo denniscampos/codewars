ecdd63d787850b145331d68fa43710f9099624b0
Dennis Campos <dennisjr89@gmail.com>
2021-02-02 13:16:53 -0500

    uploaded new challenge of the day

---
+++ 8kyu/Get the mean of an array/main.js
@@ -0,0 +1,26 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+
+function getAverage(marks){
+
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    
+    let sum = marks.reduce(reducer)
+  
+    let division = sum / marks.length
+   
+    
+    return Math.floor(division)
+  }
+  
+  
+  // completed Feb 02 2021
+
+//   A better version: 
+  function getAverage(marks){
+    return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+  }
