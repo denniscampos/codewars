8ebfac0471083ae419f2af9ed4e9cc6db121ac8d
Dennis Campos <dennisjr89@gmail.com>
2021-02-27 20:34:21 -0500

    adding my daily codewars

---
+++ 8kyu/calculate average/main.js
@@ -0,0 +1,13 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0
+
+function find_average(array) {
+  
+    let zero = 0
+    
+    for(let i = 0; i < array.length; i++) {
+      zero += array[i]
+    }
+      return zero / array.length
+  }
---
+++ 8kyu/find the max and min values of a list/main.js
@@ -0,0 +1,22 @@
+// Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+// #Examples
+// max([4,6,2,1,9,63,-134,566]) returns 566
+// min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+// max([5]) returns 5
+// min([42, 54, 65, 87, 0]) returns 0
+
+
+let min = function(list){
+    
+    return Math.min(...list); // this takes the min integer of an array
+}
+
+let max = function(list){
+    
+    return Math.max(...list); // this takes the max integer of an array
+}
+
+// better version
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
