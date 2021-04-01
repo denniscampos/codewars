c10c30974753aca362899235b6708167e51283fc
Dennis Campos <dennisjr89@gmail.com>
2021-04-01 09:52:17 -0400

    daily codewars

---
+++ 8kyu/you cant code under pressure/main.js
@@ -0,0 +1,11 @@
+/* 
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+  const double = i * 2;
+  return double;
+}
+
+// better solution
+const doubleInteger = (i) => i * 2;
