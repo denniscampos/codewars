5977c5eb92cdef0bd5ee09ab8d2d77721cd6a4a9
Dennis Campos <dennisjr89@gmail.com>
2021-04-11 21:19:02 -0400

    daily codewars

---
+++ 8kyu/Multiply/main.js
@@ -0,0 +1,18 @@
+/*
+This code does not execute properly. Try to figure out why.
+
+function multiply(a,b) {
+a * b
+}
+
+*/
+
+// Old Version
+function multiply(a, b) {
+  const multip = a * b;
+  return multip;
+}
+
+// Better version
+
+const multiply = (a, b) => a * b;
