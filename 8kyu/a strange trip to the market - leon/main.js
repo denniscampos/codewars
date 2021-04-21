5f6dbd444e4d301077671f98adef52ecdf5ae636
Dennis Campos <dennisjr89@gmail.com>
2021-04-21 17:15:05 -0400

    daily codewars

---
+++ 8kyu/a strange trip to the market - leon/main.js
@@ -0,0 +1,10 @@
+function isLockNessMonster(s) {
+  const treeFiddy = "tree fiddy";
+  const fiddyTree = 3.5;
+
+  if (s.includes(treeFiddy) || s.includes(fiddyTree)) {
+    return true;
+  } else {
+    return false;
+  }
+}
