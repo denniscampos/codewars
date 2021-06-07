ec073597ef86509a8d7dcb082b9fca20aaa164db
dnbull <dennisjr89@gmail.com>
2021-06-07 09:33:38 -0400

    daily codewars

--- 8kyu/pick a set of first elements/main.js
+++ 8kyu/pick a set of first elements/main.js
@@ -12,9 +12,9 @@ first(arr, 0) //=> [];
 */
 
 function first(arr, n = 1) {
-    if(n == 0 ) {
-        return []
-    } else if (n > 0) {
-        return arr.slice(0, n)
-    }
+  if (n == 0) {
+    return [];
+  } else if (n > 0) {
+    return arr.slice(0, n);
+  }
 }
