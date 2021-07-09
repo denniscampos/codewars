3c91915927431144938282657aa2f089a0a0ee7c
Dennis Campos <dennisjr89@gmail.com>
2021-07-09 14:42:13 -0400

    daily codewars

---
+++ 8kyu/sort and star/main.js
@@ -0,0 +1,19 @@
+/*
+You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+*/
+
+// map & slice is unnecessary.
+const twoSort = (s) =>
+  s
+    .sort()[0]
+    .split("")
+    .map((x) => x + "***")
+    .join("")
+    .slice(0, -3);
+
+// better solution
+const twoSort = (s) => s.sort()[0].split("").join("***");
